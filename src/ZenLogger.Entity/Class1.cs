8f9f937d5fd92c4ecbd20cebcf88e15f0e3b6f8f
wk-j <somnuk.wk@gmail.com>
2018-08-01 21:58:47 +0700

    Added context

--- src/ZenLogger.Entity/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ZenLogger.Entity
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/ZenLogger.Entity/ZenContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace ZenLogger.Entity {
+    public enum LogLevel {
+        Trace, Debug, Info, Error
+    }
+
+    public class LogInfo {
+        [Key]
+        public int Id { set; get; }
+        public LogLevel LogLevel { set; get; }
+        public string Message { set; get; }
+        public DateTime DateTime { set; get; } = DateTime.Now;
+        public string Application { set; get; }
+        public string Ip { set; get; }
+    }
+
+    public class ZenContext : DbContext {
+        public ZenContext(DbContextOptions options) : base(options) { }
+        public DbSet<LogInfo> LogInfos { set; get; }
+    }
+}
